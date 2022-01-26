b6b55810b80a78fc0df05ecadbfafa6a008b7911
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2022-01-26 00:16:32 -0800

    new files

---
+++ cmake-build-debug/CMakeFiles/Leetcode_Exercises.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Leetcode_Exercises.dir/two_sum.cpp.o"
+  "Leetcode_Exercises"
+  "Leetcode_Exercises.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Leetcode_Exercises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Leetcode_Exercises.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Leetcode_Exercises.
+# This may be replaced when dependencies are built.
